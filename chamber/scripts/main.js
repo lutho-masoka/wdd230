e42d1afcc9a665fd328bceb5457366363cc6bb17
Lutho Masoka <luthomasoka@gmail.com>
2022-10-15 21:45:35 +0200

    home page continued

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,9 +1,24 @@
+const displayBannerMsg = () => {
+    let currentDate = new Date().getDay();
+    let msg = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    const banner = document.querySelector('.banner-msg');
+
+    if (currentDate == 1 || currentDate == 2) {
+        document.querySelector('.banner-msg p').innerHTML = msg;
+        banner.focus();
+    } else {
+        document.querySelector('.banner-msg p').style.display = "none";
+    }
+}
+
+const weekday = displayBannerMsg();
 const todaysDate = new Date(document.lastModified);
 const dateField = document.querySelector('#dateTime');
 const date = new Date();
 const year = todaysDate.getFullYear();
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation');
+const joinBtn = document.querySelector('#join-nav-btn');
 
 const fullDate = new Intl.DateTimeFormat("en-UK", {
     dateStyle: "full"
@@ -15,4 +30,10 @@ document.getElementById("currentdate").innerHTML = todaysDate.toLocaleString('en
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
+if(joinBtn) {
+    joinBtn.addEventListener('click', () => {window.location.href = "join.html"}, false);
+}
+
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+displayBannerMsg();
